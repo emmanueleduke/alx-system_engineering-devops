1c1b9f57d8535e351740d7819c0772784c96c8fe
emmanueleduke <eduke.emmanuel@gmail.com>
2023-08-10 14:56:42 +0100

    debug error 500

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+
+#!/usr/bin/python3
+#!fest to fix a bug in wp-setings.php
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}/usr/bin/python3
